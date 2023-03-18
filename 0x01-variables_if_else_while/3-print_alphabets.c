0047dddde714a3391c5c8b0844ced054974f19cf
konyhea <ndukaprincek@gmail.com>
2023-03-17 20:38:31 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *Return: Always (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);
+	return (0);
+}
