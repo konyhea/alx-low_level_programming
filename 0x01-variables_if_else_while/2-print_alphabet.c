e5b83101916cda3b09a5718e402ea00aaa2eec71
konyhea <ndukaprincek@gmail.com>
2023-03-17 19:22:34 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * getting started with the projects
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+putchar("\n");
+return (0);
+}
