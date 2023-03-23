9b02dce72d8918b2139a7e92ebfdb00bf81a769d
konyhea <ndukaprincek@gmail.com>
2023-03-22 17:59:51 -0700

    federal agent jack bauer

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,1 +1,17 @@
 #include "main.h"
+#includee <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\m", n);
+	return (0);
+}
