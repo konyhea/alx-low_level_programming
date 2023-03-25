beb5406f174f916cc21a29052bb0fa2396b46c0d
konyhea <ndukaprincek@gmail.com>
2023-03-24 21:24:28 -0700

    revision

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j <= 14; j++)
 	{
-	if (i >= 10)
-	_putchar(i / 10 + '0');
-	_putchar(i % 10 + '0');
+	if (j >= 10)
+	_putchar(j / 10 + '0');
+	_putchar(j % 10 + '0');
 	}
 	_putchar('\n');
 	}
