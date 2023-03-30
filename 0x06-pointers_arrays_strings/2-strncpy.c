67357dac17904c580af821f4164adb88e10c3d15
konyhea <ndukaprincek@gmail.com>
2023-03-29 19:27:21 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - function to copies string
+ * @dest: string to be returned
+ * @src: string to be added
+ * @n: number of bytes
+ *
+ * Return: Always 1 success
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
