ab903376c325b1fbdd998c527638d1916406faa2
konyhea <ndukaprincek@gmail.com>
2023-03-30 18:25:17 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,9 +16,9 @@ int main(void)
 	 * - only one statement
 	 *  - you are not allowed to code anyrthing else than this line
 	 */
-	*(p + 5);
+	*(p + 5) = 98;
 	/*  ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2];
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
 
