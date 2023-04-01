985ad14a1b7ea1d71d0b1bf532e416c9d7b2db11
konyhea <ndukaprincek@gmail.com>
2023-03-31 19:37:33 -0700

    revision 0

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,17 +6,17 @@
  *
  * Return: ...
  */
-
 void print_number(int n)
 {
-	if (n < 0)
+	if (n < 10)
 	{
 		_putchar('-');
 		n = -n;
 	}
+
 	if (n / 10)
 	{
-	print_number(n / 10);
+		print_number(n / 10)
 	}
 	_putchar(n % 10 + '0');
 }
