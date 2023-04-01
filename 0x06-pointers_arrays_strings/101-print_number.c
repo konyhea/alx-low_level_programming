733b85ffdde75411203e0666e955e77e6649ed02
konyhea <ndukaprincek@gmail.com>
2023-03-31 19:50:23 -0700

    revision of magic

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_number - write a function that prints an integer
  * @n: integer to be printed
@@ -16,7 +16,7 @@ void print_number(int n)
 
 	if (n / 10)
 	{
-		print_number(n / 10)
+		print_number(n / 10);
 	}
 	_putchar(n % 10 + '0');
 }
