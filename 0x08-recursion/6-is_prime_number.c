db2c83153fb7ecd552862046113e13a79982c5bb
konyhea <ndukaprincek@gmail.com>
2023-04-05 20:54:17 -0700

    function that reverse a string in reverse

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function that check number is prime
+ * @n: n is the number to be checked
+ *
+ * Return: Always 0
+ */
+
+int is_prime_number(int n)
+{
+}
