f7f85bcaf013ca40a0e37ffb676b6d5c702ca6e5
konyhea <ndukaprincek@gmail.com>
2023-04-09 20:34:14 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: the argument counts
+ * @argv: pointer to argument
+ *
+ * Return: Always Success 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+
+
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}
