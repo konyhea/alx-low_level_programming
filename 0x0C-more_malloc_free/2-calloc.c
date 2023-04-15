e962f8ae1c75903e8466fe84c7a33f65e8fd8cea
konyhea <ndukaprincek@gmail.com>
2023-04-14 20:14:48 -0700

    everything

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i < 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 	s[i] = b;
 	}
@@ -34,8 +34,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
-	int *ptr;
+	char  *ptr;
 
 	if (nmemb == 0 || size == 0)
 	return (NULL);
