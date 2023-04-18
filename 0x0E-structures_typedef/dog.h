1891055e568439365b57ec5d7c63a7a8f75aa2dc
konyhea <ndukaprincek@gmail.com>
2023-04-18 16:35:59 -0700

    header file and declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,9 @@ struct dog
 	char *owner;
 };
 
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
@@ -24,5 +27,9 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
+
+int _strlen(char *s);
+
+char *_strcpy(char *dest, char *src);
 
 #endif
