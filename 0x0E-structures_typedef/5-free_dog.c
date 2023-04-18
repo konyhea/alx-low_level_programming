9a5a7e55f2300b8a6ac406a290103efb005b8735
konyhea <ndukaprincek@gmail.com>
2023-04-17 20:40:07 -0700

    task 5 : free the allocated memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: the struct to bd freed
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+	return;
+	}
+	free(d->name);
+	free(d->owner);
+
+	free(d);
+}
