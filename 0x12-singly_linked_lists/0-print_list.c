b2c60349ba5fffa014ec743d2ae861e4d9e2878a
konyhea <ndukaprincek@gmail.com>
2023-04-27 18:22:37 -0700

    print all the elements of a list_h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,14 +5,15 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 		printf("[0](nil)\n");
 		else
-		printf("[%u]%s\n", h->len, h->str);
+		printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
+
 	return (count);
 }
