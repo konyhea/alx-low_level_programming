882ecffd6d4eb1673809f385e9de8852f7be2cf4
konyhea <ndukaprincek@gmail.com>
2023-05-27 15:20:22 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,8 +19,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-	return (-1);
 
 
 	if  (text_content != NULL)
@@ -31,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	n_written = write(fd, text_content, n_len);
 
-	if (n_written == -1)
+	if (fd || n_written == -1)
 	return (-1);
 
 	close(fd);
