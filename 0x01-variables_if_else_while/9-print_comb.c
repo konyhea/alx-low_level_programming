9eacc7a15c1c9007c581098c6f3192af92da2b0b
konyhea <ndukaprincek@gmail.com>
2024-02-15 04:37:58 -0800

    printing combo single

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,16 +10,17 @@ int main(void)
 {
 	int i = 48;
 
-	while(i <= 57)
+	while (i <= 57)
 	{
 		putchar(i);
-		if(i != 57)
+		if (i != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 		i++;
 	}
+	putchar('\n');
 
 	return (0);
 }
