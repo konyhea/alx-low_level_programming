e0b706325b9e640b99be57e33e19e3eef88b2d27
konyhea <ndukaprincek@gmail.com>
2024-07-17 17:46:22 -0700

    ln search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,25 @@
 #include "search_algos.h"
 
+
+/**
+ * linear_recursion - recursion to find linear saerch
+ * @array: pointer to the first element in the array
+ * size: the size of the array
+ * @value: the value sought after
+ * index: position to start traversing
+ */
+
+int ln_recursion(int *array, size_t size, int value, size_t index)
+{
+	if (index == size)
+		return (-1);
+
+	printf("Value checked array[%lu] = %d\n", index, array[index]);
+
+	if (array[index] == value)
+		return (index);
+	return (ln_recursion(array, size, value,  index + 1));
+}
 /**
  * linear_search -  return first index of element in array.
  * @array: pointer to first eleent  array.
@@ -9,19 +29,9 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
 
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%lu] = %d\n", i, array[i]);
-		if (array[i] == value)
-		{
-			return (i);
-		}
-	}
-
-	return (-1);
+	return (ln_recursion(array, size, value, 0));
 }
