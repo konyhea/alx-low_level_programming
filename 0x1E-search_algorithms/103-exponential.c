44808c269b7ba7a10a5868a4c5cac21d763c32a5
konyhea <ndukaprincek@gmail.com>
2024-07-18 04:20:11 -0700

    expo typo fix

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,12 +1,13 @@
 #include "search_algos.h"
 
 /**
+ * bn_search - binary search employed in expon search
+ * @array: the first elem in the array
+ * @l: the left boundray
+ * @r: the right boundary
+ * @value: the coveted value
  *
- *
- *
- *
- *
- *
+ * Return: the index of value or - 1 if NULL
  */
 int bn_search(int *array, size_t l, size_t r, int value)
 {
@@ -16,8 +17,8 @@ int bn_search(int *array, size_t l, size_t r, int value)
 		return (-1);
 
 	printf("Searching in array: %d", array[i++]);
-	while(i <= r)
-		printf(",  %d", array[i++]);
+	while (i <= r)
+		printf(", %d", array[i++]);
 	printf("\n");
 
 	mid = l + ((r - l) / 2);
@@ -38,9 +39,13 @@ int bn_search(int *array, size_t l, size_t r, int value)
 }
 
 /**
+ * exponential_search - using exponentail method to find value
+ * @array: the first element in the array
+ * @size: the number of elements in the array
+ * @value: the sought after value
  *
  *
- *
+ * Return: index of value or - 1 if not found
  */
 
 int exponential_search(int *array, size_t size, int value)
@@ -49,7 +54,7 @@ int exponential_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	
+
 	while (r < size && array[r] < value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", r, array[r]);
@@ -58,11 +63,12 @@ int exponential_search(int *array, size_t size, int value)
 
 	if (array[r] == value)
 		return (r);
-	 l = r / 2;
+	l = r / 2;
 
-	 if (r >= size)
-		 r = size - 1;
-	 printf("Value found between indexes [%lu] and [%lu]\n", l, r);
 
-	 return (bn_search(array, l, r, value));
+	if (r >= size)
+		r = size - 1;
+		printf("Value found between indexes [%lu] and [%lu]\n", l, r);
+
+	return (bn_search(array, l, r, value));
 }
