0e8d40b559aafcd89808f3bf1d3b68f28f8804d8
konyhea <ndukaprincek@gmail.com>
2024-07-17 18:36:59 -0700

    space coplexity

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,10 +2,12 @@
 #define __SEARCH_ALGOS_H
 /* standard libraries used */
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 
 /* functions pro-types */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
