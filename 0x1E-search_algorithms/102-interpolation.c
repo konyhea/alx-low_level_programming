619a082a4ea8b9575df2890443cb15453ecbfa4a
konyhea <ndukaprincek@gmail.com>
2024-07-17 19:16:14 -0700

    interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,7 +12,7 @@
 
 int int_recursion(int *array, size_t low, size_t high, int value)
 {
-	 size_t pos = low + (((double)(high - low) / (array[high] - array[low]))\
+	 size_t pos = low + (((double)(high - low) / (array[high] - array[low]))
 			 * (value - array[low]));
 
 	if (pos > high)
@@ -48,5 +48,5 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	if (array == NULL)
 		return (-1);
-	return int_recursion(array, 0, size - 1, value);
+	return (int_recursion(array, 0, size - 1, value));
 }
